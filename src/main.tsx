b8f51b07b4bc59bfd4cf3e3d0e1045f1e4107d3a
Eduardo-EGO1 <eduardogomes.job@outlook.com>
2025-09-28 23:10:59 -0300

    feat: core project files

---
+++ src/main.tsx
@@ -0,0 +1,33 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./components/login/index.tsx";
+import Profile from "./components/profile/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
